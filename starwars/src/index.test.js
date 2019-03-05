10a557db2e17dc51ea4c17780f62d2d0d6148477
nhuvuong <kelly.mavier@gmail.com>
2019-03-05 21:50:27 +0100

    update index.test.js

--- starwars/src/index.test.js
+++ starwars/src/index.test.js
@@ -7,25 +7,31 @@ describe('starwars-names', () => {
       expect(starWars.all).toHaveLength(starWarsNames.length)
     });
     test('should be an array of strings', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(starWars.all).toContain()
     });
 
     test('should contain `Luke Skywalker`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(starWars.all).toContain('Luke Skywalker');
     });
 
     test('should not contain `Ben Quadinaros`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(starWars.all).not.toContain('Ben Quadinaros');
+
     });
   });
 
   describe('random', () => {
     test('should return a random item from the starWars.all', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      const randomItem = starWars.random()
+
+      expect(starWars.all).toContain(randomItem);
     });
 
     test('should return an array of random items if passed a number', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      const number = 3
+      const arrayRandom = starWars.random(number)
+
+      expect(starWars.all).toIncludeAllMembers(arrayRandom);
     });
   });
 });
